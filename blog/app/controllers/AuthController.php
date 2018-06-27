28fd452e453ff9b680c736e6759a7b3bd4a852d0
Ivan <ivancardozo11@hotmail.com>
2018-06-27 00:55:28 -0300

    Autenticacion de usuario completa para la seccion admin

--- blog/app/controllers/AuthController.php
+++ blog/app/controllers/AuthController.php
@@ -33,4 +33,10 @@ class AuthController extends BaseController {
     ]);
 
   }
+
+  public function getLogout(){
+    unset($_SESSION['userId']);
+    header('Location:' . BASE_URL. 'auth/login');
+  }
+
 }
